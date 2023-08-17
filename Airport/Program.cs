0da63bbba2687a79e9290e3fd76e562556a6e965
Mohammed Buirat <1192896@student.birzeit.edu>
2023-08-17 12:59:45 +0300

    Implement main

--- Airport/Program.cs
+++ Airport/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ManagerLogIn();
+        }
+
+        static void ManagerLogIn()
+        {
+            ManagerValidation managerValidation = new ManagerValidation();
+            managerValidation.SetValidation();
+            LoadFlights loadFlights = new LoadFlights();
+            bool valid;
+            List<Flight> flights = loadFlights.LoadData(out valid);
+            if (valid)
+            {
+                CustomerLogIn(flights);
+            }
+            else
+            {
+                Console.WriteLine("Data file is facing some errors fix them to match the desired constraints");
+            }
+        }
+
+        static void CustomerLogIn(List<Flight> flights)
+        {
+            List<Booking> bookings = new List<Booking>();
+            CustomerOperations customerOperations = new CustomerOperations();
+            Console.WriteLine("Welcome to AirPalestina");
+            customerOperations.CustomerMain(flights, bookings);
+
         }
     }
 }
