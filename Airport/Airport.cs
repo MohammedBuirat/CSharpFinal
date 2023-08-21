948c9910042accc133268e532fb26722a6f81612
Mohammed Buirat <1192896@student.birzeit.edu>
2023-08-21 18:35:38 +0300

    Implement Airport

---
+++ Airport/Airport.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport
+{
+    internal class Airport
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+
+        public Airport(string name, string country, string city)
+        {
+            Name = name;
+            Country = country;
+            City = city;
+        }
+
+        public override string ToString()
+        {
+            return $"Airport Name: {Name}   Country: {Country}  City: {City}";
+        }
+    }
+}
