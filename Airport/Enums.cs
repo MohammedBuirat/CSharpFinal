b8cdc3973e0c16afe20870521d4fc20eb406ad32
Mohammed Buirat <1192896@student.birzeit.edu>
2023-08-21 18:23:03 +0300

    Implement enums

---
+++ Airport/Enums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport
+{
+    public enum FlightClass
+    {
+        Economy,
+        Business,
+        FirstClass
+    }
+
+    [Flags]
+    enum FlightStatus
+    {
+        Valid = 0,
+        IdReserved = 1,
+        DepartureDateError = 2,
+        ArrivalDateError = 4,
+        NumberOfSeatsOutOfBound = 8,
+        FlightPriceOutOfBound = 16
+    }
+}
