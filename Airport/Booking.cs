25269a52fddc8aac82e2117e1cc5a898912b415f
Mohammed Buirat <1192896@student.birzeit.edu>
2023-08-21 19:55:15 +0300

    Implement Booking

---
+++ Airport/Booking.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Airport
+{
+    internal class Booking
+    {
+        //to store the number of booking made so far
+        static int NumOfBooking = 1;
+
+        public int Id { get; init; }
+        public FlightClass FlightClass { get; set; }
+        public Flight Flight { get; set; }
+        public int Seat { get; set; }
+        public DateTime BookingTime { get; init; }
+        public decimal BookingPrice { get; set; }
+
+        public Booking(FlightClass flightClass, Flight flight, int seat)
+        {
+            Id = NumOfBooking;
+            NumOfBooking++;
+            Flight = flight;
+            BookingTime = DateTime.Now;
+            Seat = seat;
+            FlightClass = flightClass;
+            BookingPrice = SetBookingPrice();
+            Flight.BookSeat(seat);
+        }
+
+        public decimal SetBookingPrice()
+        {
+            if (FlightClass == FlightClass.Business) return Flight.BusniessClassPrice;
+            else if (FlightClass == FlightClass.FirstClass) return Flight.FirstClassPrice;
+            else return Flight.EconomyClassPrice;
+        }
+
+        public override string ToString()
+        {
+            return $"Booking id: {Id}  Flight: {Flight.ToString()}     Flight class: {FlightClass}    Booking price: {BookingPrice}   Seat num: {Seat}    Booking time: {BookingTime}";
+        }
+    }
+}
